5830acfb9d056d2ad745b5dfbcd399c4263ecf6d
BigTeacherMan <152217556+BigTeacherMan@users.noreply.github.com>
2024-06-06 10:46:25 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function changeText() {
+   document.getElementById("button").innerHTML = "<h1>HEADING</h1>";
+   document.getElementById("button").style.backgroundColor = "rgb(0, 210, 150)";
+}
+
+
+function changeColor(elementID, color) {
+    const element = document.getElementById(elementID);
+
+    if (element && element.id === "button") {
+        alert("This worked!")
+    }
+
+    element.style.color = color;
+}
+
+
+function hoverSize(elementId) {
+  const element = document.getElementById(elementId);
+
+  element.style.transform = "scale(1.2";
+}
+
+
+function checkAndChangeStyle(elementId) {
+    const element = document.getElementById(elementId);
+
+    if (element && element.id === "background") {
+        if (element.style.backgroundColor === "blue") {
+            element.style.backgroundColor = 'red';
+            alert("Background color changed to red!");
+          } 
+        else if (element.style.backgroundColor === "red") {
+            element.style.backgroundColor = 'green';
+            alert("Background color changed to green!");
+          }
+        else if (element.style.backgroundColor === "green") {
+            element.style.backgroundColor = "yellow";
+        }
+        else {
+            element.style.backgroundColor = 'blue';
+            alert("Background color changed to blue!");
+          }
+        }
+  }
+
